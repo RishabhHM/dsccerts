a7738fd08ab1f52763984ff57dbd12cce204dbb1
RishabhHM <49658231+RishabhHM@users.noreply.github.com>
2019-04-22 18:04:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return render_template('base.html')
 
+@app.route('/')
+def hello1():
+    return "Rishabh"
+
 if __name__ == '__main__':
     app.run(debug=True)
